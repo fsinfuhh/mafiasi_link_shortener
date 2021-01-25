576c0938f72b5974145efe674e515caa47e00442
Finn-Thorben Sell <dev@finn-thorben.me>
2021-01-25 14:01:57 +0100

    add test case for non-changing shorts

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
 
-# Create your tests here.
+from links import models
+from . import serializers
+
+
+class SerializationTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.user = get_user_model().objects.create(username="test")
+
+    def test_short_cannot_be_changed(self):
+        link = models.Link.objects.create(short="test", target="https://example.com", owner=self.user)
+        serializer = serializers.LinkSerializer(instance=link, data={
+            "short": "something-different",
+        }, context={})
+        with self.assertRaises(ValidationError):
+            serializer.is_valid(raise_exception=True)
