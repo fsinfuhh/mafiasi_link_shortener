ca0e76548c8249f171eb1dba07f0d083d6549bb4
Finn-Thorben Sell <dev@finn-thorben.me>
2021-01-27 19:35:07 +0100

    fix some serializer bugs

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
 from links import models
 
 
@@ -14,8 +14,10 @@ class LinkSerializer(serializers.HyperlinkedModelSerializer):
         min_length=6,
         max_length=32,
         default=serializers.CreateOnlyDefault(models.link_default_short),
+        validators=[validators.UniqueValidator(queryset=models.Link.objects.all())]
     )
     owner = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    target = serializers.URLField(max_length=200, required=True)
 
     def save(self, **kwargs):
         # set the current user as default owner for all operations but allow explicit overwriting with
