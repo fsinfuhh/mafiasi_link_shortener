0ad64735d5a0ae06188676f139ef582fbc725a8a
Finn-Thorben Sell <dev@finn-thorben.me>
2021-01-27 19:34:54 +0100

    add more serializer tests

--- src/api/tests.py
+++ src/api/tests.py
@@ -3,10 +3,8 @@ import json
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from rest_framework.exceptions import ValidationError
 
 from links import models
-from . import serializers
 
 
 class SerializationTestCase(TestCase):
@@ -16,16 +14,55 @@ class SerializationTestCase(TestCase):
         cls.user = get_user_model().objects.create(username="test")
 
     def test_short_cannot_be_changed(self):
+        c = Client()
+        c.force_login(self.user)
         link = models.Link.objects.create(short="test", target="https://example.com", owner=self.user)
-        serializer = serializers.LinkSerializer(
-            instance=link,
-            data={
-                "short": "something-different",
-            },
-            context={},
+        response = c.patch(
+            reverse("link-detail", kwargs={"short": link.short}),
+            data=json.dumps({"short": "something-different"}),
+            content_type="application/json",
         )
-        with self.assertRaises(ValidationError):
-            serializer.is_valid(raise_exception=True)
+        self.assertEqual(response.status_code, 400)
+
+    def test_owner_gets_set(self):
+        c = Client()
+        c.force_login(self.user)
+        with self.subTest(msg="owner is not given"):
+            response = c.post(
+                reverse("link-list"),
+                data=json.dumps({"short": "test123", "target": "https://example.com"}),
+                content_type="application/json",
+            )
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(response.json()["owner"], self.user.username)
+            self.assertEqual(models.Link.objects.get(short="test123").owner, self.user)
+
+        with self.subTest(msg="owner is given"):
+            other_user = get_user_model().objects.create(username="other_user")
+            response = c.post(
+                reverse("link-list"),
+                data=json.dumps(
+                    {
+                        "short": "test1234",
+                        "target": "https://example.com",
+                        "owner": other_user.username,
+                    }
+                ),
+                content_type="application/json",
+            )
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(response.json()["owner"], self.user.username)
+            self.assertEqual(models.Link.objects.get(short="test123").owner, self.user)
+
+    def test_link_with_existing_short_cannot_be_created(self):
+        c = Client()
+        c.force_login(self.user)
+        link = models.Link.objects.create(short="test123", target="https://example.com", owner=self.user)
+        response = c.post(reverse("link-list"), data=json.dumps({
+            "short": link.short,
+            "target": "https://other.example.com",
+        }), content_type="application/json")
+        self.assertNotEqual(response.status_code, 201)
 
 
 class PermissionTestCase(TestCase):
@@ -39,7 +76,7 @@ class PermissionTestCase(TestCase):
     def test_noauth_api_cannot_be_accessed(self):
         c = Client()
         response = c.get(reverse("link-list"))
-        self.assertEqual(response.status_text.lower(), "forbidden")
+        self.assertEqual(response.status_code, 403)
 
     def test_listed_links_includes_only_own(self):
         c = Client()
@@ -57,17 +94,20 @@ class PermissionTestCase(TestCase):
         c = Client()
         url = reverse("link-detail", kwargs={"short": self.link.short})
         for (user, should_succeed) in [(self.user1, True), (self.user2, False)]:
-            with self.subTest(msg=f"user={user.username}"):
-                c.force_login(user)
+            c.force_login(user)
 
-                with self.subTest(msg="http get"):
-                    response = c.get(url)
-                    self.assertEqual(response.status_code, 200 if should_succeed else 403)
+            with self.subTest(msg=f"should_succeed={should_succeed}, http get"):
+                response = c.get(url)
+                self.assertEqual(response.status_code, 200 if should_succeed else 403)
 
-                with self.subTest(msg="http patch"):
-                    response = c.patch(url, data=json.dumps({"target": "https://example.example.com"}), content_type="application/json")
-                    self.assertEqual(response.status_code, 200 if should_succeed else 403)
+            with self.subTest(msg=f"should_succeed={should_succeed}, http patch"):
+                response = c.patch(
+                    url,
+                    data=json.dumps({"target": "https://example.example.com"}),
+                    content_type="application/json",
+                )
+                self.assertEqual(response.status_code, 200 if should_succeed else 403)
 
-                with self.subTest(msg="http delete"):
-                    response = c.delete(url)
-                    self.assertEqual(response.status_code, 204 if should_succeed else 403)
+            with self.subTest(msg=f"should_succeed={should_succeed}, http delete"):
+                response = c.delete(url)
+                self.assertEqual(response.status_code, 204 if should_succeed else 403)
