9e3bd73bbbf21d22f7e4b18e1342ad380aebfba4
Finn-Thorben Sell <dev@finn-thorben.me>
2022-12-01 16:40:51 +0100

    configure django logging to log to console

--- src/mafiasi_link_shortener/settings.py
+++ src/mafiasi_link_shortener/settings.py
@@ -193,7 +193,20 @@ SPECTACULAR_SETTINGS = {
     },
 }
 
-# Configurable properties
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
 SECRET_KEY = env.str("SHORTLINK_SECRET_KEY")
 ALLOWED_HOSTS = env.list("SHORTLINK_ALLOWED_HOSTS")
 
