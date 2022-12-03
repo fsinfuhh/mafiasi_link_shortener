6e0e6eb2ebdc3bb2b5ac71cbfa7042df8f444de0
Finn-Thorben Sell <dev@finn-thorben.me>
2022-12-03 15:51:41 +0100

    add redirect to /app when visiting /

--- src/mafiasi_link_shortener/urls.py
+++ src/mafiasi_link_shortener/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/app/")),
     path("admin/", admin.site.urls),
     path("api/", include("mafiasi_link_shortener.api.urls")),
     path("", include("mafiasi_link_shortener.frontend.urls")),
