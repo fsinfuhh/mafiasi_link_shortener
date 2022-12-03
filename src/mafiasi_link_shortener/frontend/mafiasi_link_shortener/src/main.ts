ccffa3b000cec3e4a87f716b99fb26e4b4d4ab67
Finn-Thorben Sell <dev@finn-thorben.me>
2022-12-03 13:54:23 +0100

    enable vuetify treeshaking

--- src/mafiasi_link_shortener/frontend/mafiasi_link_shortener/src/main.ts
+++ src/mafiasi_link_shortener/frontend/mafiasi_link_shortener/src/main.ts
@@ -5,14 +5,8 @@ import App from "./App.vue";
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 import { createPinia } from "pinia";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+const vuetify = createVuetify();
 
 // pinia
 const pinia = createPinia();
