5afc903f50b6075fe2d01dee9befa335842e608e
Timon Engelke <timon.engelke@online.de>
2023-06-22 13:04:53 +0200

    update whitenoise middleware

--- src/mafiasi_link_shortener/frontend/views.py
+++ src/mafiasi_link_shortener/frontend/views.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-from django.conf import settings
 from django.views import View
 from whitenoise.middleware import WhiteNoiseMiddleware
 
@@ -9,42 +8,38 @@ DIST_DIR = Path(__file__).absolute().parent / "mafiasi_link_shortener" / "dist"
 
 class FrontendServer(WhiteNoiseMiddleware):
     """
-    A server based on a middleware which uses whitenoise to efficiently serve the built frontend files.
+    A middleware which uses whitenoise to efficiently serve the built frontend files.
+
     It also serves index.html for all unknown urls which is
     `necessary when using the HTML5 History Mode API <https://router.vuejs.org/guide/essentials/history-mode.html>`_.
     """
 
-    def configure_from_settings(self, settings):
-        super().configure_from_settings(settings)
+    def __init__(self):
+        super().__init__()
         self.static_prefix = "/app/"
         self.static_root = str(DIST_DIR)
         self.autorefresh = False
         self.use_finders = False
 
-    def immutable_file_test(self, path, url):
-        # get file name by removing the static_prefix
-        name = url[len(self.static_prefix) :]
+        # add all files inside the apps dist directory
+        self.add_files(self.static_root, self.static_prefix)
 
-        # return False if the file name does not have a hash component
-        name_without_hash = self.get_name_without_hash(name)
-        if name == name_without_hash:
-            return False
-
-        return True
-
-    def process_request(self, request):
-        result = super().process_request(request)
+    def __call__(self, request):
+        static_file = self.files.get(request.path_info)
 
         if (
-            result is None
+            static_file is None
             and self.url_is_canonical(request.path_info)
             and request.path_info.startswith(self.static_prefix)
         ):
-            result = self.serve(
-                self.files.get(self.static_prefix + "index.post.html"), request
+            static_file = self.files.get(f"{self.static_prefix}/index.html")
+
+        if static_file is None:
+            raise ValueError(
+                f"cannot serve request {request.path_info} because there is no file to back it"
             )
 
-        return result
+        return self.serve(static_file, request)
 
 
 class AppView(View):
@@ -53,7 +48,7 @@ class AppView(View):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.server = FrontendServer(settings=settings)
+        self.server = FrontendServer()
 
     def get(self, request):
-        return self.server.process_request(request)
+        return self.server(request)
